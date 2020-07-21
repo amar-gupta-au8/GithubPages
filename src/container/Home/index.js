13e5f1214c446e56cc8e04d4969c5e44328c76d6
Amar Gupta <amar9911056196@gmail.com>
2020-07-21 13:22:56 +0530

    first commit

---
+++ src/container/Home/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react';
+import useFetchJobs from '../../utils/useFetchjobs';
+import { Container } from 'react-bootstrap';
+import Job from '../../components/Job';
+
+const Home = () => {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+  const { jobs, loading, error } = useFetchJobs(params, page);
+  return (
+    <Container className='my-4'>
+      <h1>Github Jobs</h1>
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>error... Try refreshing</h1>}
+      {jobs.map((job) => (
+        <Job key={job.id} job={job} />
+      ))}
+    </Container>
+  );
+};
+
+export default Home;
