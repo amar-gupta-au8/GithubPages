822b0fee075f80d0c72c0f561ee9c0522b0d5f20
Amar Gupta <amar9911056196@gmail.com>
2020-07-21 12:04:37 +0530

    first commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,19 @@
 import React, { useState } from 'react';
 import useFetchJobs from '../utils/useFetchjobs';
 import { Container } from 'react-bootstrap';
+import Job from '../components/Job';
 
 const App = () => {
-  const { jobs, loading, error } = useFetchJobs();
-  const [params, setParams] = useState('');
-  const [page, setPage] = useState('');
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState({});
+  const { jobs, loading, error } = useFetchJobs(params, page);
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>error... Try refreshing</h1>}
+      {jobs.map((job) => (
+        <Job key={job.id} job={job}/>
+      ))}
       <h1>{jobs.length}</h1>
     </Container>
   );
